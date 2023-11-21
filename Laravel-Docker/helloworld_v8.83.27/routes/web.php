5119078b2a82f47a549a103740643adbdccb0c0f
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-21 08:30:31 -0500

    [UPD] routes new

--- Laravel-Docker/helloworld_v8.83.27/routes/web.php
+++ Laravel-Docker/helloworld_v8.83.27/routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::view('/', 'landing.home')->name('home');
+Route::view('/home', 'landing.home')->name('home');
