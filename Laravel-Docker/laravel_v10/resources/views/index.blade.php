b9e81408cf84b79396ea45d5d52161daec0e3b69
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-22 13:02:54 -0500

    [UPD] extends of layout

--- Laravel-Docker/laravel_v10/resources/views/index.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/index.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Home</h1>
+@extends('layouts.landing')
+@section('title', 'Home')
+@section('content')
+    <h1>Home | index</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique deserunt, voluptate distinctio aliquam nemo rerum tempora necessitatibus laboriosam, eligendi enim, architecto aperiam dolore quod magnam repellendus. Fuga sequi ipsa illo!</p>
-</body>
-</html>
+
+    <h2>Nos vemos pronto!</h2>
+@endsection
