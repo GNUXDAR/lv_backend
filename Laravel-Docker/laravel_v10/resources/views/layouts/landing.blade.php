9150dfd74dd2e6a5c73af15f6ea07801f84a98c8
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-22 18:16:47 -0500

    [UPD] add partials

--- Laravel-Docker/laravel_v10/resources/views/layouts/landing.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/layouts/landing.blade.php
@@ -5,9 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Layout de Laravel')</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+    @yield('styles')
+    <title>
+        @yield('title')
+    </title>
 </head>
 <body>
     @include('layouts.partials.menu')
-    @yield('content')
+    <main>
+        @yield('content')
+    </main>
+    
+    @yield('scripts')
 </body>
 </html>
