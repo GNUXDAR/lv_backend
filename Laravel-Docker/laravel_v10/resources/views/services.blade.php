270f46e83b476252844da06b95d0997eb8fb9b09
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-22 18:16:00 -0500

    [UPD] add components

--- Laravel-Docker/laravel_v10/resources/views/services.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/services.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.landing')
 @section('title', 'Service')
 @section('content')
-    <h1>Home | Service</h1>
+    <h1>Service</h1>
+    <div class="cards">
+        @component('components.card')
+            @slot('title', 'service 1') 
+            @slot('content', 'Lorem ipsum dolor set aimet.Lorem ipsum dolor set aimet.Lorem ipsum dolor set aimet.Lorem ipsum dolor set aimet.Lorem ipsum dolor set aimet.')       
+        @endcomponent
+        @component('components.card')
+            @slot('title', 'service 2') 
+            @slot('content', 'Lorem ipsum dolor set aimet.')       
+        @endcomponent
+            @component('components.card')
+            @slot('title', 'service 3') 
+            @slot('content', 'Lorem ipsum dolor set aimet.')       
+        @endcomponent
+    </div>
 @endsection
