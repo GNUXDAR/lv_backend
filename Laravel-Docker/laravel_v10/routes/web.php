50a41e9c5cc1d5c7a3e982f2f859134e25957fb8
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-23 18:19:13 -0500

    [UPD] updated and add routes with controllers

--- Laravel-Docker/laravel_v10/routes/web.php
+++ Laravel-Docker/laravel_v10/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController; //add
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// only views
 Route::view('/home', 'index')->name('home');
 Route::view('/sobre-nosotros', 'about')->name('about');
 Route::view('/services', 'services')->name('services');
 Route::view('/contact', 'contact')->name('contact');
+
+// Controllers
+Route::get('/users', [UserController::class, 'index'])->name('user.index');
+// antes era...Route::get('/users', 'UserController@index')->name('user.index');
+
+Route::get('/create', [UserController::class, 'create'])->name('user.create');
