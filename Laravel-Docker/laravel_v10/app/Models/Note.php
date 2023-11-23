4a36ce2109c1989f99d661e2d9c12bd9acb33842
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-23 13:05:17 -0500

    [NEW] models file

---
+++ Laravel-Docker/laravel_v10/app/Models/Note.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "content",
+        "deadline",
+        "done"
+    ];
+    // protected $table = "notes"
+    // protected $guarded = 
+    // protected $casts = 
+    // protected $hidden =
+}
