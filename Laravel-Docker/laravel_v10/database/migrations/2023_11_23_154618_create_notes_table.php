2dcdf29d365746933ffa99f1af1a64ed38380699
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-23 13:05:09 -0500

    [NEW] migrations file

--- Laravel-Docker/laravel_v10/database/migrations/2023_11_23_154618_create_notes_table.php
+++ Laravel-Docker/laravel_v10/database/migrations/2023_11_23_154618_create_notes_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255)->nullable();
+            $table->string('content', 255);
+            $table->date('deadline');
+            $table->boolean('done')->default(false);
+            // $table->integer('example');
+            // $table->unsignedInteger('example');
+            // $table->bigInteger('example');
+            // $table->unsignedBigInteger('example');
+            // $table->text('example');
+            // $table->float('example');
+            // $table->double('example');
+            // $table->enum('state', ['DRAFT', 'PUBLISHED', 'DELETED']);
             $table->timestamps();
         });
     }
