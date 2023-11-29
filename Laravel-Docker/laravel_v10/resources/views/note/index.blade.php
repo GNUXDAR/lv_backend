6dd078c3c23cc8867890799e38e0eb03d8923c6b
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-29 16:48:17 -0500

    [UPD] add route in link

--- Laravel-Docker/laravel_v10/resources/views/note/index.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/note/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <a href="{{ route('note.create')}}">Create New Note</a>
     <ul>
         @forelse($notes as $note)
             <li><a href="#">{{ $note->title }}</a></li>
