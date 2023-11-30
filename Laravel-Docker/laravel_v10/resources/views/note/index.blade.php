39b37245b9fefa88996eb9e1a8abc37a162d07d7
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-29 19:26:49 -0500

    [UPD] add link to show note

--- Laravel-Docker/laravel_v10/resources/views/note/index.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/note/index.blade.php
@@ -4,7 +4,7 @@
     <a href="{{ route('note.create')}}">Create New Note</a>
     <ul>
         @forelse($notes as $note)
-            <li><a href="#">{{ $note->title }}</a></li>
+            <li><a href="{{ route('note.show', $note->id)}}">{{ $note->title }}</a> | <a href="{{ route('note.edit', ['note' => $note->id]) }}">Edit</a> | <a href="">Delete</a></li>
         @empty
             <h3>No hay data.</h3>
         @endforelse
