d562da0b99089f9b772c940fbb6039dd936bdfa5
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-30 13:17:02 -0500

    [UPD] add new link with routes

--- Laravel-Docker/laravel_v10/resources/views/layouts/partials/menu.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/layouts/partials/menu.blade.php
@@ -5,6 +5,9 @@
                 <li><a href="{{ route('contact') }}">Contact</a></li>
                 <li><a href="{{ route('services') }}">Services</a></li>
                 <li><a href="{{ route('about') }}">About</a></li>
+                <li><a href="{{ route('user.index') }}">Users</a></li>
+                <li><a href="{{ route('note.index') }}">Note</a></li>
+                <li><a href="{{ route('author.index') }}">Author</a></li>
             </ul>
         </nav>
     </header>
