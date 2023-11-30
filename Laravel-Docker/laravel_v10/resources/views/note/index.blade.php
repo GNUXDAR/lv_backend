09d87c21e2ecf4d2c6643944e9d5cdfb873b031a
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-29 19:41:05 -0500

    [UPD] add method to delete note

--- Laravel-Docker/laravel_v10/resources/views/note/index.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/note/index.blade.php
@@ -4,7 +4,15 @@
     <a href="{{ route('note.create')}}">Create New Note</a>
     <ul>
         @forelse($notes as $note)
-            <li><a href="{{ route('note.show', $note->id)}}">{{ $note->title }}</a> | <a href="{{ route('note.edit', ['note' => $note->id]) }}">Edit</a> | <a href="">Delete</a></li>
+            <li>
+                <a href="{{ route('note.show', $note->id)}}">{{ $note->title }}</a> | 
+                <a href="{{ route('note.edit', ['note' => $note->id]) }}">Edit</a> | 
+                <form method="POST" action="{{ route('note.destroy', $note->id) }}">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Delete">
+                </form>
+            </li>
         @empty
             <h3>No hay data.</h3>
         @endforelse
