1bf71eddb3b016000a59e5295819317475fae0d8
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-30 13:16:43 -0500

    [UPD] add menu in  layout app

--- Laravel-Docker/laravel_v10/resources/views/layouts/app.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/layouts/app.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+    @yield('styles')
     <title>Document</title>
 </head>
 <body>
+    @include('layouts.partials.menu')
     @include('layouts.partials.messages')
     @yield('content')
 </body>
