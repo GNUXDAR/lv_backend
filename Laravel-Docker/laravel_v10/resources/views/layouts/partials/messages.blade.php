51ae632e57478da1c35a2db0a908b85539f00b37
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-30 17:16:26 -0500

    [UPD] session::has in flash messages

--- Laravel-Docker/laravel_v10/resources/views/layouts/partials/messages.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/layouts/partials/messages.blade.php
@@ -1,8 +1,16 @@
 @if($message = Session::get('success'))
-    <div style="padding:15px; background-color: green; color:#fff;">
+    <div style="padding:15px; background-color: blue; color:#fff;">
         <p>{{ $message }}</p>
     </div>
 @endif
+
+@if(Session::has('success'))
+    <div class="alert alert-success" role="alert" style="padding:15px; background-color: orange; color:#fff;">
+        {{ Session::get('success') }}
+    </div>
+@endif
+
+
 @if($message = Session::get('danger'))
     <div style="padding:15px; background-color: red; color:#fff;">
         <p>{{ $message }}</p>
