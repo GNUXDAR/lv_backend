fd4117881148367c0d230d982781eab001a97801
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-11-29 21:54:43 -0500

    [UPD] show errors

--- Laravel-Docker/laravel_v10/resources/views/note/create.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/note/create.blade.php
@@ -1,17 +1,36 @@
 @extends('layouts.app')
 @section('content')
+
 <a href="{{ route('note.index')}}">Back</a>
 <form action="{{ route('note.store')}}" method="POST">
     @csrf
     <label for="">Title</label>
-    <input type="text" name="title">
+    <input type="text" name="title"> <br>
+    @error('title')
+        <code>{{ $message }}</code>
+    @enderror
 
     <label for="">Content</label>
-    <input type="text" name="content">
+    <input type="text" name="content"> <br>
+    @error('content')
+        <code>{{ $message }}</code>
+    @enderror
 
     <label for="">Author</label>
-    <input type="text" name="author">
+    <input type="text" name="author"> <br>
+    @error('content')
+        <code>{{ $message }}</code>
+    @enderror
 
     <input type="submit" value="Save">
 </form>
+
+{{-- mostrar todos los errores --}}
+@if($errors->any())
+<ul>
+    @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+    @endforeach
+</ul>
+@endif
 @endsection
