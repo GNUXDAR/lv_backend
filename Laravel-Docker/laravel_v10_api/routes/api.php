eed818314f50e121bcab1fb215e9d492caa9e6cb
Arturo Cabrera <6179849+GNUXDAR@users.noreply.github.com>
2023-12-01 23:39:35 -0500

    [UPD] routes API

--- Laravel-Docker/laravel_v10_api/routes/api.php
+++ Laravel-Docker/laravel_v10_api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController; // controller Note
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// los prefijos podemos agregarlos a las rutas apis como a las rutas web
+Route::prefix('/v1')->resource('/note', NoteController::class);
