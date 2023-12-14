2088a236bbf3a4df466593e12dab86461bdf5929
gnuxdar <6179849+GNUXDAR@users.noreply.github.com>
2023-12-14 12:58:26 -0500

    [UPD] add data of a new phone to example HasMany

--- Laravel-Docker/laravel_v10/database/seeders/PhoneSeeder.php
+++ Laravel-Docker/laravel_v10/database/seeders/PhoneSeeder.php
@@ -18,5 +18,12 @@ class PhoneSeeder extends Seeder
             'phone_number' => 1234567,
             'user_id' => 3
         ]);
+
+        // otro dato para la relacion uno a muchos
+        Phone::create([
+            'prefix' => 58,
+            'phone_number' => 7654321,
+            'user_id' => 3
+        ]);
     }
 }
