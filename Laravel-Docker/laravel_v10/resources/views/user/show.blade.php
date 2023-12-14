c87ebd8d6e216fb91fc70c22c85467c5b74fd0ed
gnuxdar <6179849+GNUXDAR@users.noreply.github.com>
2023-12-14 09:51:16 -0500

    [UPD] shoy many numbers phone

--- Laravel-Docker/laravel_v10/resources/views/user/show.blade.php
+++ Laravel-Docker/laravel_v10/resources/views/user/show.blade.php
@@ -5,6 +5,14 @@
     <h1>Nombre:{{ $user->name }}</h1>
 
     <p>Estos datos de numero es una relacion de modelos</p>
-    <h3>Prefijo: {{ $user->phone->prefix }}</h3>
-    <h3>Numero: {{ $user->phone->phone_number }}</h3>
+    {{-- <h3>Prefijo: {{ $user->phone->prefix }}</h3> --}}
+    <h3>Numero: 
+        <ul>
+            {{-- un usuario puede tener varios numero hasMany --}}
+            @foreach ($user->phones as $phone)
+            <li>{{ $phone->prefix }} {{ $phone->phone_number }}</li>
+                
+            @endforeach
+        </ul>
+        {{-- {{ $user->phone->phone_number }}</h3> --}}
 @endsection
