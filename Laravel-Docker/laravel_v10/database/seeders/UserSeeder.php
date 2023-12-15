631ed176abd8fa64c668ee26d2346add12ef811b
gnuxdar <6179849+GNUXDAR@users.noreply.github.com>
2023-12-15 08:07:08 -0500

    [UPD] new data to example relationship belongsToMany

--- Laravel-Docker/laravel_v10/database/seeders/UserSeeder.php
+++ Laravel-Docker/laravel_v10/database/seeders/UserSeeder.php
@@ -16,10 +16,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // los usuarios anteriores se usaron en el controller para un ejemplo
         User::create([
             'id' => 3, 
             'name' => 'example',
             'email' => 'example@example.com', 
+            'password' => Hash::make('1234567'),
+            'age' => 20,
+            'address' => 'Tunapuy',
+            'zip_code' => 7654321
+        ]);
+
+        // para el ejemplo de la relacino muchos a muchos
+        User::create([
+            'id' => 4,
+            'name' => 'example2',
+            'email' => 'example2@example.com',
+            'password' => Hash::make('1234567'),
+            'age' => 20,
+            'address' => 'Tunapuy',
+            'zip_code' => 7654321
+        ]);
+
+        User::create([
+            'id' => 5,
+            'name' => 'example3',
+            'email' => 'example3@example.com',
             'password' => Hash::make('1234567'),
             'age' => 20,
             'address' => 'Tunapuy',
