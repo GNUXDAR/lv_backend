49f0faa34316e8668d90d470b6051321465ee62b
gnuxdar <6179849+GNUXDAR@users.noreply.github.com>
2024-02-05 09:42:29 -0500

    [NEW] login - form

--- Laravel-Docker/laravel_v10/routes/web.php
+++ Laravel-Docker/laravel_v10/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController; //User Controller
-use App\Http\Controllers\NoteController; //Note Controller
-use App\Http\Controllers\AuthorController; // Author Controller
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,33 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// only views
-Route::view('/home', 'index')->name('home');
-Route::view('/sobre-nosotros', 'about')->name('about');
-Route::view('/services', 'services')->name('services');
-Route::view('/contact', 'contact')->name('contact');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Controllers
-Route::get('/users', [UserController::class, 'index'])->name('user.index');
-// antes era...Route::get('/users', 'UserController@index')->name('user.index');
-Route::get('/user/show/{user}', [UserController::class, 'show'])->name('user.show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/create', [UserController::class, 'create'])->name('user.create');
-
-// Rutas Dinamicas
-// Route::get('/note/{id?}', [NoteController::class, 'index'])->name('note.index');
-
-// Rutas de Note CRUD
-Route::get('/note', [NoteController::class, 'index'])->name('note.index');
-Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
-Route::post('/note/store', [NoteController::class, 'store'])->name('note.store');   // Guardar nota
-Route::get('/note/edit/{note}', [NoteController::class, 'edit'])->name('note.edit');   // Editar nota
-Route::put('/note/update/{note}', [NoteController::class, 'update'])->name('note.update');  // Actualizar nota
-Route::get('/note/show/{note}', [NoteController::class, 'show'])->name('note.show');    // Mostrar nota
-Route::delete('/note/detroy/{note}', [NoteController::class, 'destroy'])->name('note.destroy');  // Eliminar nota
-
-// Rutas de Author CRUD
-Route::resource('/author', AuthorController::class);
-
-// Rutas de productos, para mostrar los seeder
-Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+require __DIR__.'/auth.php';
